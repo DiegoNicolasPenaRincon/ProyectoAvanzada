ba21ad7d8186e47332966d23900c152174af36d8
DiegoNicolasPenaRincon <146769438+DiegoNicolasPenaRincon@users.noreply.github.com>
2025-04-06 22:58:12 -0500

    Creacion de la base del proyecto final

---
+++ ProyectoFinalAvanzada/src/main/java/org/uniquindio/proyectofinalavanzada/ProyectoFinalAvanzadaApplication.java
@@ -0,0 +1,13 @@
+package org.uniquindio.proyectofinalavanzada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFinalAvanzadaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoFinalAvanzadaApplication.class, args);
+    }
+
+}
