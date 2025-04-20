ead9b68e64286c73aa0bfb14a0d19b2c9f8b0e7d
DiegoNicolasPenaRincon <146769438+DiegoNicolasPenaRincon@users.noreply.github.com>
2025-04-20 17:17:09 -0500

    se finalizo la implementacion del repositorio de notificaciones

--- ProyectoFinalAvanzada/src/main/java/org/uniquindio/proyectofinalavanzada/repositories/NotificacionRepository.java
+++ ProyectoFinalAvanzada/src/main/java/org/uniquindio/proyectofinalavanzada/repositories/NotificacionRepository.java
@@ -1,9 +1,17 @@
 package org.uniquindio.proyectofinalavanzada.repositories;
 
+import jakarta.validation.constraints.NotNull;
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 import org.uniquindio.proyectofinalavanzada.domain.Notificacion;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface NotificacionRepository extends MongoRepository<Notificacion, String> {
+    Optional<Notificacion> findNotificacionById(String id);
+    List<Notificacion> findAllByUsuarioId(@NotNull ObjectId usuarioId);
+
 }
--- ProyectoFinalAvanzada/src/main/java/org/uniquindio/proyectofinalavanzada/services/NotificacionServiceImpl.java
+++ ProyectoFinalAvanzada/src/main/java/org/uniquindio/proyectofinalavanzada/services/NotificacionServiceImpl.java
@@ -1,12 +1,15 @@
 package org.uniquindio.proyectofinalavanzada.services;
 
 import lombok.RequiredArgsConstructor;
+import org.bson.types.ObjectId;
 import org.springframework.stereotype.Service;
 import org.uniquindio.proyectofinalavanzada.domain.Notificacion;
 import org.uniquindio.proyectofinalavanzada.dtos.NotificacionDTO;
 import org.uniquindio.proyectofinalavanzada.dtos.NotificacionResponseDTO;
 import org.uniquindio.proyectofinalavanzada.exception.ResourceNotFoundException;
+import org.uniquindio.proyectofinalavanzada.exception.ValueConflictException;
 import org.uniquindio.proyectofinalavanzada.mappers.NotificacionMapper;
+import org.uniquindio.proyectofinalavanzada.repositories.NotificacionRepository;
 
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
@@ -18,8 +21,57 @@ public class NotificacionServiceImpl implements NotificacionService {
 
     private final Map<String, Notificacion> notificacionStore = new ConcurrentHashMap<>();
     private final NotificacionMapper notificacionMapper;
+    private final NotificacionRepository repositorioNotificacion;
+
 
     @Override
+    public NotificacionResponseDTO enviarNotificacion(NotificacionDTO notificacionDTO) {
+        if(repositorioNotificacion.findNotificacionById(notificacionDTO.reporteId()).isPresent())
+        {
+            throw new ValueConflictException("Esa notificacion ya se encuentra creada");
+        }
+        Notificacion notificacion = notificacionMapper.toNotificacion(notificacionDTO);
+        repositorioNotificacion.save(notificacion);
+        return notificacionMapper.toNotificacionResponseDTO(notificacion);
+    }
+
+    @Override
+    public List<NotificacionResponseDTO> listarNotificacionesUsuario(String usuarioId) {
+        ObjectId usuariIdBuscableEnMongo=new ObjectId(usuarioId);
+        List<Notificacion> listaBuenaNotificaciones=new ArrayList<>();
+        listaBuenaNotificaciones=repositorioNotificacion.findAllByUsuarioId(usuariIdBuscableEnMongo);
+        List<NotificacionResponseDTO> listaDevuelta=new ArrayList<>();
+        for(int i=0;i<listaBuenaNotificaciones.size();i++)
+        {
+            listaDevuelta.add(notificacionMapper.toNotificacionResponseDTO(listaBuenaNotificaciones.get(i)));
+        }
+        return listaDevuelta;
+    }
+
+    @Override
+    public void marcarNotificacionComoLeida(String id) throws Exception {;
+        if (repositorioNotificacion.findNotificacionById(id).isEmpty())
+        {
+            throw new ResourceNotFoundException("Notificación no encontrada");
+        }
+        Notificacion notificacion=repositorioNotificacion.findNotificacionById(id).get();
+        notificacion.setLeida(true);
+        repositorioNotificacion.save(notificacion);
+    }
+
+    @Override
+    public NotificacionResponseDTO obtenerNotificacion(String id) {
+        Optional<Notificacion> notificacionBacana=repositorioNotificacion.findById(id);
+        if(notificacionBacana.isEmpty())
+        {
+            throw new ResourceNotFoundException("Notificación no encontrada");
+        }
+        Notificacion notificacion=notificacionBacana.get();
+        return notificacionMapper.toNotificacionResponseDTO(notificacion);
+
+    }
+
+    /*@Override
     public NotificacionResponseDTO enviarNotificacion(NotificacionDTO notificacionDTO) throws Exception {
         Notificacion notificacion = notificacionMapper.toNotificacion(notificacionDTO);
         notificacionStore.put(notificacion.getId(), notificacion);
@@ -49,4 +101,6 @@ public class NotificacionServiceImpl implements NotificacionService {
                 .map(notificacionMapper::toNotificacionResponseDTO)
                 .orElseThrow(() -> new ResourceNotFoundException("Notificación no encontrada"));
     }
+
+     */
 }
