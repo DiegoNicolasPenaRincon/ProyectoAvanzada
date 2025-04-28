0e3827d6bb5e532ad102991f327fe4df5da74ea3
DiegoNicolasPenaRincon <146769438+DiegoNicolasPenaRincon@users.noreply.github.com>
2025-04-28 08:05:49 -0500

    Se agregaron cositas xd

--- ProyectoFinalAvanzada/src/main/java/org/uniquindio/proyectofinalavanzada/services/UsuarioService.java
+++ ProyectoFinalAvanzada/src/main/java/org/uniquindio/proyectofinalavanzada/services/UsuarioService.java
@@ -1,5 +1,6 @@
 package org.uniquindio.proyectofinalavanzada.services;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.uniquindio.proyectofinalavanzada.dtos.*;
 
 import java.util.List;
@@ -11,5 +12,6 @@ public interface UsuarioService {
     LoginResponseDTO login(LoginDTO loginDTO) throws Exception;
     UsuarioResponseDTO editarPerfil(String id, UsuarioEditarDTO usuarioEditarDTO) throws Exception;
     void eliminarPerfil(String id, boolean confirmar) throws Exception;
+    @PreAuthorize("hasAuthority('ADMIN') or @securityService.isCurrentUser(#id)")
     Optional<UsuarioResponseDTO> obtenerUsuario(String id);
 }
