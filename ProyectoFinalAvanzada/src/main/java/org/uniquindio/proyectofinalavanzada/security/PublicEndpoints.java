acefc88ed695a68b1faffb858258234deeac7203
Bou <mariac.aranzazur@uqvirtual.edu.co>
2025-05-11 02:11:53 -0500

    Update PublicEndpoints.java

--- ProyectoFinalAvanzada/src/main/java/org/uniquindio/proyectofinalavanzada/security/PublicEndpoints.java
+++ ProyectoFinalAvanzada/src/main/java/org/uniquindio/proyectofinalavanzada/security/PublicEndpoints.java
@@ -9,8 +9,8 @@ import org.springframework.stereotype.Component;
 @Getter
 public final class PublicEndpoints {
     private final RequestMatcher[] matchers = new RequestMatcher[]{
-            new AntPathRequestMatcher("/public/**"),
-            new AntPathRequestMatcher("/login", HttpMethod.POST.name()),
-            new AntPathRequestMatcher("/users", HttpMethod.POST.name())
+            new AntPathRequestMatcher("/api/auth/register", HttpMethod.POST.name()),
+            new AntPathRequestMatcher("/api/auth/verify", HttpMethod.POST.name()),
+            new AntPathRequestMatcher("/api/auth/login", HttpMethod.POST.name()),
     };
 }
